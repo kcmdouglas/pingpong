2323ea4b597a51092a1feaaef2751d9bf1b2976a
kcmdouglas <kcmdouglas@gmail.com>
2015-11-12 19:55:38 -0800

    Add functional user interface in code, changed spec test code variable to test more accurate variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var pingPong = function(userNumber) {
-  if ((userNumber % 3 === 0) || (userNumber % 5 === 0)) {
+var pingPong = function(i) {
+  if ((i % 3 === 0) || (i % 5 === 0)) {
     return true;
   } else {
     return false;
@@ -7,12 +7,22 @@ var pingPong = function(userNumber) {
 };
 
 $(function() {
-  $("form#game").submit(function(event) {
-    $("#outputList").empty();
+  $("#game").submit(function(event) {
+    $('#outputList').empty();
 
-    var number = parseInt($("#userNumber").val())
-    var output = pingPong(number)
-    
+    var number = parseInt($("#userNumber").val());
+
+    for (var i = 1; i <= number; i += 1) {
+      if (i % 15 === 0) {
+        $('#outputList').append("<li>ping pong</li>");
+      } else if (i % 3 === 0) {
+        $('#outputList').append("<li>ping</li>");
+      } else if (i % 5 === 0) {
+        $('#outputList').append("<li>pong</li>");
+      } else {
+        $('#outputList').append("<li>" + i + "</li>");
+      }
+    }
     event.preventDefault();
   });
 });
