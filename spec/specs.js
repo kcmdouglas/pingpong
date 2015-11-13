1fc986bcaa92fe62db1d707a8596b16b2744eadf
kcmdouglas <kcmdouglas@gmail.com>
2015-11-13 13:42:04 -0800

    Add new tests for isPingPong and revise tests for pingPong

--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,23 @@
 describe('pingPong', function() {
-  it("returns true for a number that is divisible by 3", function() {
-    expect(pingPong(6)).to.equal(true);
+  it("returns ping for a number that is divisible by 3", function() {
+    expect(pingPong(6)).to.equal("ping");
   });
-  it("returns true for a number that is divisible by 5", function() {
-    expect(pingPong(10)).to.equal(true);
+  it("returns pong for a number that is divisible by 5", function() {
+    expect(pingPong(10)).to.equal("pong");
   });
-  it("returns true for a number that is divisible by 3 and 5", function() {
-    expect(pingPong(30)).to.equal(true)
+  it("returns pingpong for a number that is divisible by 3 and 5", function() {
+    expect(pingPong(30)).to.equal("pingpong")
   });
   it("is false for a number that is not divisible by 3 or 5", function() {
-    expect(pingPong(8)).to.equal(false);
+    expect(!pingPong).to.equal(false);
+  });
+});
+
+describe('isPingPong', function() {
+  it("returns true for a number divisible by 3, 5, or 15", function() {
+    expect(isPingPong(6)).to.equal(true);
+  });
+  it("returns false for a number not divisible by 3, 5, or 15", function() {
+    expect(isPingPong(7)).to.equal(false);
   });
 });
