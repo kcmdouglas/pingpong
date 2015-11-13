e64e0e8cd275c108d9766d4062d4eab77984212f
kcmdouglas <kcmdouglas@gmail.com>
2015-11-12 18:41:25 -0800

    Add a test for userNumbers not divisible by 3 or 5

--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,7 @@ describe('pingPong', function() {
   it("is true for a number that is divisible by 5", function() {
     expect(pingPong(10)).to.equal(true);
   });
+  it("is false for a number that is not divisible by 3 or 5", function() {
+    expect(pingPong(8)).to.equal(false);
+  });
 });
