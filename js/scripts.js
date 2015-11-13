84639f7c85a9ae4109dee5b042e44fecc16c02dd
kcmdouglas <kcmdouglas@gmail.com>
2015-11-13 15:15:20 -0800

    Remove code for pingPong, leaving pingPongType and isPingPong as functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,16 @@
-var pingPong = function(i) {
-  if (isPingPong(i)) {
-    return pingPongType(i)
+var pingPongType = function(i) {
+  if (i % 15 === 0) {
+      return "pingpong";
+  } else if (i % 5 === 0) {
+      return "pong";
   } else {
-    return false;
+      return "ping";
   }
 }
 
-var pingPongType = function(i) {
-  if ((i % 3 === 0) && (i % 5 != 0)) {
-      return "ping";
-    } else if ((i % 5 === 0) && (i % 3 !=0)) {
-      return "pong";
-    } else if (i % 15 === 0){
-      return "pingpong";
-    }
-  }
-
 var isPingPong = function(i) {
-  if ((i % 5 === 0) || (i % 3 === 0)){
-    return true;
+  if ((i % 5 === 0) || (i % 3 === 0) || (i % 15 === 0)) {
+    return pingPongType(i);
   } else {
     return false;
   }
@@ -32,7 +24,7 @@ $(function() {
     var whichPingPong = pingPongType(i)
 
     for (var i = 1; i <= number; i += 1) {
-      if (pingPong(i)) {
+      if (isPingPong(i)) {
         $('#outputList').append("<li>" + whichPingPong + "</li>");
       } else {
         $('#outputList').append("<li>" + i + "</li>");
