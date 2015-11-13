469d1407397d602bfa00d3229707b321029f9d93
kcmdouglas <kcmdouglas@gmail.com>
2015-11-12 18:35:41 -0800

    Removed previous test due to redundancy

--- spec/specs.js
+++ spec/specs.js
@@ -2,13 +2,7 @@ describe('pingPong', function() {
   it("is true for a number that is divisible by 3", function() {
     expect(pingPong(6)).to.equal(true);
   });
-
   it("is true for a number that is divisible by 5", function() {
     expect(pingPong(10)).to.equal(true);
   });
-
-  it("is true for a number that is divisible by 15", function() {
-    expect(pingPong(30)).to.equal.(true);
-  });
-
 });
