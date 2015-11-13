de2fb2c260720ae3142066f2a3b611b7ba922af6
kcmdouglas <kcmdouglas@gmail.com>
2015-11-12 20:43:44 -0800

    Change spec test according to new javascript test code

--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,12 @@
 describe('pingPong', function() {
-  it("is true for a number that is divisible by 3", function() {
-    expect(pingPong(6)).to.equal(true);
+  it("returns ping for a number that is divisible by 3", function() {
+    expect(pingPong(6)).to.equal("ping");
   });
-  it("is true for a number that is divisible by 5", function() {
-    expect(pingPong(10)).to.equal(true);
+  it("returns pong for a number that is divisible by 5", function() {
+    expect(pingPong(10)).to.equal("pong");
+  });
+  it("returns ping pong for a number that is divisible by 3 and 5", function() {
+    expect(pingPong(30)).to.equal("ping pong")
   });
   it("is false for a number that is not divisible by 3 or 5", function() {
     expect(pingPong(8)).to.equal(false);
