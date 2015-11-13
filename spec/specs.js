9c7c32327c5c33ae2799c133f5b8a1f7a21acb60
kcmdouglas <kcmdouglas@gmail.com>
2015-11-12 17:16:50 -0800

    Add specs.js file with initial spec test for pingPong function

---
+++ spec/specs.js
@@ -0,0 +1,10 @@
+describe('pingPong', function() {
+  it("is true for a number that is divisible by 3", function() {
+    expect(pingPong(6)).to.equal(true);
+  });
+
+  it("is true for a number that is divisible by 5", function() {
+    expect(pingPong(10)).to.equal(True)
+  }
+
+});
