2241f38a62026a64e0f22ea8c776843db9cd02b7
kcmdouglas <kcmdouglas@gmail.com>
2015-11-13 14:38:24 -0800

    Add new test called pingPongType

--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,24 @@
 describe('pingPong', function() {
-  it("returns ping for a number that is divisible by 3", function() {
+  it("is false for a number that is not divisible by 3 or 5", function() {
+    expect(pingPong(7)).to.equal(false);
+  });
+  it("will run pingPongType if isPingPong is true", function() {
     expect(pingPong(6)).to.equal("ping");
   });
+});
+
+describe('pingPongType', function() {
+  it("returns ping for a number that is divisible by 3", function() {
+    expect(pingPongType(6)).to.equal("ping");
+  });
   it("returns pong for a number that is divisible by 5", function() {
-    expect(pingPong(10)).to.equal("pong");
+    expect(pingPongType(10)).to.equal("pong");
   });
   it("returns pingpong for a number that is divisible by 3 and 5", function() {
-    expect(pingPong(30)).to.equal("pingpong")
-  });
-  it("is false for a number that is not divisible by 3 or 5", function() {
-    expect(!pingPong).to.equal(false);
+    expect(pingPongType(30)).to.equal("pingpong")
   });
 });
+
 
 describe('isPingPong', function() {
   it("returns true for a number divisible by 3, 5, or 15", function() {
