dd92650577e3fae7b1d0e647251b4753da1e2258
kcmdouglas <kcmdouglas@gmail.com>
2015-11-14 12:22:35 -0800

    Add comments to function and loop for clarity

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+/*This function will take the updated index number
+from my loop, apply these rules, and then output
+ping, pong, pingpong or the index number into
+the outputList */
 var pingPongType = function(i) {
   if ((i % 5 === 0) || (i % 3 === 0) || (i % 15 === 0)) {
     if (i % 15 === 0) {
@@ -23,6 +27,9 @@ $(function() {
       alert(warning);
     }
 
+/*This loop will count from 1 to the user input number,
+run each index number through the pingPongType function,
+then output the results into the outputList*/
     for (var i = 1; i <= number; i += 1) {
       var output = pingPongType(i)
       $('#outputList').append("<li>" + output + "</li>")
